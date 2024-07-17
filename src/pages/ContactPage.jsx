62f2735dd6190b46828378083e09e90d36a081e6
comaco <taro0koma@gmail.com>
2024-07-17 16:15:13 +0900

    見やすく

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -225,7 +225,7 @@ const ContactPage = () => {
               onChange={handleChangeOption}
               className="p-2 border border-gray-300 rounded"
             >
-              <option value="">--Select--</option>
+              <option value="">気持ちや感想を選択してください</option>
               <option value="ほっとした">ほっとした</option>
               <option value="おどろいた">おどろいた</option>
               <option value="うれしい">うれしい</option>
@@ -243,13 +243,19 @@ const ContactPage = () => {
               onChange={handleChangeGrade}
               className="p-2 border border-gray-300 rounded"
             >
-              <option value="">--Select Grade--</option>
-              <option value="1年生">1年生</option>
-              <option value="2年生">2年生</option>
-              <option value="3年生">3年生</option>
-              <option value="4年生">4年生</option>
-              <option value="5年生">5年生</option>
-              <option value="6年生">6年生</option>
+              <option value="">学年を選択してください</option>
+              <option value="小学1年生">小学1年生</option>
+              <option value="小学2年生">小学2年生</option>
+              <option value="小学3年生">小学3年生</option>
+              <option value="小学4年生">小学4年生</option>
+              <option value="小学5年生">小学5年生</option>
+              <option value="小学6年生">小学6年生</option>
+              <option value="中学1年生">中学1年生</option>
+              <option value="中学2年生">中学2年生</option>
+              <option value="中学3年生">中学3年生</option>
+              <option value="高校1年生">高校1年生</option>
+              <option value="高校2年生">高校2年生</option>
+              <option value="高校3年生">高校3年生</option>
             </select>
           </div>
           <button
