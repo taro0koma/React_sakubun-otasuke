b94d9862c7356501ea78869075431548f052631c
comaco <taro0koma@gmail.com>
2024-07-20 15:23:27 +0900

    リセット

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import env from "vite-plugin-env-compatible";
 // import './src/assets/css/index.css';
 
-// tailWind
 // import {VitePluginTailwind} from 'vite-plugin-tailwind';
 
 // https://vitejs.dev/config/
