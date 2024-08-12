20c37e09ab8725dac2009b407297fb8f75ea9586
comaco <taro0koma@gmail.com>
2024-08-12 10:47:52 +0900

    動画をClipchampからとってきた

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -14,17 +14,7 @@ const AboutPage = () => {
         <ModalFrame title="イメージマップの作り方について" text="頭で考えても思いつかないのに紙に書いてみると、ふしぎなくらいイメージがわいてくる！何を書けばいいか思いつかないときは、紙の真ん中に1つだけ気持ちを書いてスタートしよう（「面白かった」「面白くなかった」からでもだいじょうぶ）" onClose={handleModalClose} imageSrc="/vite.svg"/>
       )}
       <p></p>
-      {/* <iframe 
-        width="560" 
-        height="315" 
-        src="https://www.youtube.com/embed/RYrGVCtLXik?si=Hhuu8ZoVJ997pboP" 
-        title="YouTube video player" 
-        frameBorder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerPolicy="strict-origin-when-cross-origin" 
-        allowFullScreen>
-      </iframe> */}
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/UmVWOEZAq0I?si=uusYe1HjzSuAsgJq&amp;start=74" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <video controls src="/images/figjamvideo.mp4" style={{width:"100%"}}></video>
     </div>
   );
 };
