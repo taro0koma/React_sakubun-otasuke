2aaf74ce5be046eb4ef81709a9f39138cf044e09
comaco <taro0koma@gmail.com>
2025-07-06 14:46:56 +0900

    プロンプトを修正

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -161,7 +161,7 @@ const ContactPage = () => {
   }
 
   async function fetchAIResponse(option, grade, examples) {
-    const userMessage = `下記の言葉を${option}という意味合いでどんなときにも使える拡張した言葉を10個つくり、それを改行のある1．２．などの表示になるよう箇条書きにしてください。：\n\n${examples}`;
+    const userMessage = `下記の言葉を${option}という意味合いでどんなときにも使える拡張した言葉を10個つくり、それを改行のある1．２．などの表示になるよう箇条書きにしてください。3.〇〇や-〇〇な〇〇-のような当てはめるべきところは当てはめて出力してください。：\n\n${examples}`;
     
     try {
       const response = await fetch(process.env.REACT_APP_API_URL + "/api/azure", {
